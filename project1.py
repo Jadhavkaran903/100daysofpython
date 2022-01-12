d54d80d1d2c88f0ed1b6cb36f3eca5fa2a8592a4
paradox903 <jadhavkaran903@gmail.com>
2022-01-13 00:13:04 +0530

    First commit

---
+++ project1.py
@@ -0,0 +1,6 @@
+# team name generator
+
+print("Welcome to team name generator")
+city = input("enter your city name")
+animal = input("enter animal name")
+print("Your team name is " + city +" "+ animal)
